6d6119ce039eb75bedfd34053cf5e96878460431
Alberto González <agonzalez@tribalyte.com>
2020-11-18 19:19:23 +0100

    Initial Commit

---
+++ CitiesTableExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CitiesTableExercise
+//
+//  Created by Alberto González Hernández on 18/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
